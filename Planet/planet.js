e82342458d8341f9a2cc4f9f70f4c984e7933e15
Zach <zmclean11@gmail.com>
2022-11-01 07:40:26 -0500

    updated planet

--- Planet/planet.js
+++ Planet/planet.js
@@ -1,4 +1,4 @@
-var modelViewMatrix);
+var modelViewMatrix;
 var modelViewMatrixLoc;
 var projectionMatrix;
 var projectionMatrixLoc;
@@ -20,8 +20,8 @@ window.onload = function init()
 
     // Generate the points for the 3 components of the planet
     GenerateBackCircles();
-    GenerateCircle();
-    GenerateFrontCircles();
+    // GenerateCircle();
+    // GenerateFrontCircles();
 
     modelViewMatrix = mat4();
     projectionMatrix = ortho(-8, 8, -8, 8, -1, 1);
@@ -67,14 +67,39 @@ function scale4(a, b, c) {
 function GenerateBackCircles()
 {
 
-
+    var radius=0.5;
+    var center = scale4(1, 1.618, 1);
+    // right blue
+    GenerateCircle(center, radius);
+    
+    // center = vec2(-0.3, 0);
+    // // blue yellow
+    // GenerateCircle(center, radius);
+    //
+    // // left blend circle
+    // radius=0.2;
+    // center = vec2(-0.3, .75);
+    // GenerateCircle(center, radius);
+    //
+    // // right blend circle
+    // center = vec2(0.3, .75);
+    // GenerateCircle(center, radius);
 
 }
 
-function GenerateCircle()
+function GenerateCircle(center, radius)
 {
 
+	SIZE=100; // slices
 
+	var angle = 2*Math.PI/SIZE;
+	
+    // Because LINE_STRIP is used in rendering, SIZE + 1 points are needed 
+    // to draw SIZE line segments 
+    points.push(center);
+    for  (var i=0; i<SIZE+1; i++) {
+        points.push([center[0]+radius*Math.cos(i*angle), center[1]+radius*Math.sin(i*angle)]);
+    }
 
 
 }
@@ -92,8 +117,10 @@ function DrawFullPlanet()
 
     // Draw Back Circles
 
-
-
+    // blue circle
+    // gl.uniform1i(gl.getUniformLocation(program, "colorIndex"), 2);
+    // gl.drawArrays( gl.TRIANGLE_FAN, 0, SIZE+2);
+	gl.drawArrays( gl.TRIANGLE_FAN, 0, 4);
     // draw planet circle
 
  
